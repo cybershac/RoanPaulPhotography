9ccf506515286134832bb6e9f87b358d17486950
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-10-09 11:00:13 +1000

    fixed for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,9 +34,9 @@ function scrollActive(){
         sectionId = current.getAttribute('id');
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         } else {
-            document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
